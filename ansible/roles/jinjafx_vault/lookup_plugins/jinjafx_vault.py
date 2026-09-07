9a03ad6441178bf68e6d1f71d5451055de64ff6e
Chris Mason <Chris.Mason@virginmediao2.co.uk>
2026-09-07 11:36:51 +0100

    update

--- ansible/roles/jinjafx_vault/lookup_plugins/jinjafx_vault.py
+++ ansible/roles/jinjafx_vault/lookup_plugins/jinjafx_vault.py
@@ -4,20 +4,36 @@ from ansible.errors import AnsibleError
 
 class LookupModule(LookupBase):
   def run(self, terms, variables=None, **kwargs):
+    headers = { 'X-Vault-Token': variables['jinjafx_vault_login']['json']['token'] }
+    verify = variables.get('jinjafx_vault_verify', True)
+    timeout = variables.get('jinjafx_vault_timeout', 5)
+
+    if not verify:
+      requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+
     if len(terms) == 2:
-      headers = { 'X-Vault-Token': variables['jinjafx_vault_login']['json']['token'] }
-      verify = variables.get('jinjafx_vault_verify', True)
-      timeout = variables.get('jinjafx_vault_timeout', 5)
       namespace = terms[0]
       variable = terms[1]
-
-      if not verify:
-        requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
       if (r := requests.get(variables['jinjafx_vault_url'] + f'/v1/data/{namespace}/{variable}', headers=headers, verify=verify, timeout=timeout)).status_code == 200:
         return [json.loads(r.text)['data']]
 
       else:
-        raise AnsibleError(f'Unable to lookup JinjaFx Vault variable "{variable}" within namespace "{namespace}"')
+        raise AnsibleError(f'JinjaFx Vault - Unable to get variable "{variable}" within namespace "{namespace}"')
 
-    raise AnsibleError('Invalid arguments provided to JinjaFx Vault lookup')
+    elif len(terms) == 1:
+      namespace = terms[0]
+
+      if (r := requests.get(variables['jinjafx_vault_url'] + f'/v1/data/{namespace}', headers=headers, verify=verify, timeout=timeout)).status_code == 200:
+        obj = json.loads(r.text)
+        result = {}
+
+        for k, v in obj.items():
+          result[k] = v['data']
+
+        return [result]
+
+      else:
+        raise AnsibleError(f'JinjaFx Vault - Unable to get namespace "{namespace}"')
+
+    raise AnsibleError('JinjaFx Vault - Invalid arguments provided to lookup function')
